b439adb80fba60332a8e4295a3793a94e96fad7f
Kevin Taulman <kevin.taulman@icloud.com>
2019-05-21 14:38:59 -0700

    yes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import {without} from 'lodash';
+console.log(without);
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
